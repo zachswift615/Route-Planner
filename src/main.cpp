128c7c60523ce4f38f005b8d6746a01f499745de
Zach Swift <cras.zswift@gmail.com>
2021-09-30 09:56:32 -0500

    clean comments in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -53,9 +53,7 @@ int main(int argc, const char **argv)
             osm_data = std::move(*data);
     }
     
-    // TODO 1: Declare floats `start_x`, `start_y`, `end_x`, and `end_y` and get
-    // user input for these values using std::cin. Pass the user input to the
-    // RoutePlanner object below in place of 10, 10, 90, 90.
+    // Get user input for coordinates and pass them to route_planner
     float start_x, start_y, end_x, end_y;
     char c;
     cout << "Enter start x and y coordinates as 'x,y'\n";
